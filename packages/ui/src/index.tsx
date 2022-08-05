c7f1efb0834be81340c4cf51a3e9396dd59b4e12
ImaginaSergio <sergio@imaginagroup.com>
2022-08-05 12:49:02 +0200

    feat: Añadiendo dependencias de ui

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import * as React from 'react';
 
-// export * from './lib';
-// export * from './form';
-// export * from './core';
-// export * from './theme';
+export * from './lib';
+export * from './form';
+export * from './core';
+export * from './theme';
