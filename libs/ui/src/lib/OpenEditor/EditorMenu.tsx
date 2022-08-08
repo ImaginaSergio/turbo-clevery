5e776491091d88b4f4abe0e8578a23d515b59198
ImaginaSergio <sergio@imaginagroup.com>
2022-08-08 09:47:53 +0200

    limpieza

--- libs/ui/src/lib/OpenEditor/EditorMenu.tsx
+++ libs/ui/src/lib/OpenEditor/EditorMenu.tsx
@@ -1,17 +1,18 @@
 import { useCallback } from 'react';
+
 import {
-  BiHeading,
-  BiBold,
-  BiItalic,
-  BiStrikethrough,
-  BiCodeBlock,
-  BiParagraph,
-  BiLink,
-  BiListOl,
-  BiListUl,
   BiCode,
   BiUndo,
   BiRedo,
+  BiBold,
+  BiLink,
+  BiListOl,
+  BiListUl,
+  BiItalic,
+  BiHeading,
+  BiCodeBlock,
+  BiParagraph,
+  BiStrikethrough,
 } from 'react-icons/bi';
 import { Flex, Icon, IconButton } from '@chakra-ui/react';
 
@@ -42,217 +43,124 @@ export const EditorMenu = ({ editor, isDisabled = false }: EditorMenuProps) => {
   }, [editor]);
 
   return (
-    <Flex
-      wrap="wrap"
-      w="100%"
-      bg="gray_1"
-      h="fit-content"
-      roundedTop="xl"
-      align="center"
-      justify="flex-end"
-      pr="10px"
-    >
+    <Flex wrap="wrap" w="100%" bg="gray_1" h="fit-content" roundedTop="xl" align="center" justify="flex-end" pr="10px">
       <IconButton
         p="0px"
         bg="transparent"
         aria-label="Titulo"
         isDisabled={isDisabled}
-        icon={
-          <Icon
-            as={BiHeading}
-            boxSize="20px"
-            color={editor.isActive('heading') ? 'primary' : 'black'}
-          />
-        }
+        icon={<Icon as={BiHeading} boxSize="20px" color={editor.isActive('heading') ? 'primary' : 'black'} />}
         onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
-        style={{ padding: '0' }}
       />
 
       <IconButton
         p="0px"
-        isDisabled={isDisabled}
+        className="icon"
+        bg="transparent"
         aria-label="Párrafo"
-        bg="transparent"
-        icon={
-          <Icon
-            as={BiParagraph}
-            boxSize="20px"
-            color={editor.isActive('paragraph') ? 'primary' : 'black'}
-          />
-        }
-        className="icon"
+        isDisabled={isDisabled}
         onClick={() => editor.chain().focus().setParagraph().run()}
-        style={{ padding: '0' }}
+        icon={<Icon as={BiParagraph} boxSize="20px" color={editor.isActive('paragraph') ? 'primary' : 'black'} />}
       />
 
       <IconButton
         p="0px"
-        isDisabled={isDisabled}
+        className="icon"
+        bg="transparent"
         aria-label="Negrita"
-        bg="transparent"
-        icon={
-          <Icon
-            as={BiBold}
-            boxSize="20px"
-            color={editor.isActive('bold') ? 'primary' : 'black'}
-          />
-        }
-        className="icon"
+        isDisabled={isDisabled}
         onClick={() => editor.chain().focus().toggleBold().run()}
-        style={{ padding: '0' }}
+        icon={<Icon as={BiBold} boxSize="20px" color={editor.isActive('bold') ? 'primary' : 'black'} />}
       />
 
       <IconButton
         p="0px"
-        isDisabled={isDisabled}
+        className="icon"
+        bg="transparent"
         aria-label="Cursiva"
-        bg="transparent"
-        icon={
-          <Icon
-            as={BiItalic}
-            boxSize="20px"
-            color={editor.isActive('italic') ? 'primary' : 'black'}
-          />
-        }
-        className="icon"
+        isDisabled={isDisabled}
         onClick={() => editor.chain().focus().toggleItalic().run()}
-        style={{ padding: '0' }}
+        icon={<Icon as={BiItalic} boxSize="20px" color={editor.isActive('italic') ? 'primary' : 'black'} />}
       />
 
       <IconButton
         p="0px"
-        isDisabled={isDisabled}
+        className="icon"
+        bg="transparent"
         aria-label="Tachado"
-        bg="transparent"
-        icon={
-          <Icon
-            as={BiStrikethrough}
-            boxSize="20px"
-            color={editor.isActive('strike') ? 'primary' : 'black'}
-          />
-        }
-        className="icon"
+        isDisabled={isDisabled}
         onClick={() => editor.chain().focus().toggleStrike().run()}
-        style={{ padding: '0' }}
+        icon={<Icon as={BiStrikethrough} boxSize="20px" color={editor.isActive('strike') ? 'primary' : 'black'} />}
       />
 
       <IconButton
         p="0px"
-        isDisabled={isDisabled}
+        className="icon"
+        bg="transparent"
         aria-label="Código"
-        bg="transparent"
-        icon={
-          <Icon
-            as={BiCode}
-            boxSize="20px"
-            color={editor.isActive('code') ? 'primary' : 'black'}
-          />
-        }
-        className="icon"
-        onClick={() => editor.chain().focus().toggleCode().run()}
-        style={{ padding: '0' }}
-      />
-
-      <IconButton
-        p="0px"
         isDisabled={isDisabled}
-        aria-label="Enlace"
-        bg="transparent"
-        icon={
-          <Icon
-            as={BiLink}
-            boxSize="20px"
-            color={editor.isActive('link') ? 'primary' : 'black'}
-          />
-        }
-        className="icon"
-        onClick={setLink}
-        style={{ padding: '0' }}
+        onClick={() => editor.chain().focus().toggleCode().run()}
+        icon={<Icon as={BiCode} boxSize="20px" color={editor.isActive('code') ? 'primary' : 'black'} />}
       />
 
       <IconButton
         p="0px"
+        className="icon"
+        bg="transparent"
+        aria-label="Enlace"
+        isDisabled={isDisabled}
+        icon={<Icon as={BiLink} boxSize="20px" color={editor.isActive('link') ? 'primary' : 'black'} />}
+        onClick={setLink}
+      />
+
+      <IconButton
+        p="0px"
+        className="icon"
+        bg="transparent"
         isDisabled={isDisabled}
         aria-label="Lista ordenada"
-        bg="transparent"
-        icon={
-          <Icon
-            as={BiListOl}
-            boxSize="20px"
-            color={editor.isActive('orderedList') ? 'primary' : 'black'}
-          />
-        }
-        className="icon"
+        icon={<Icon as={BiListOl} boxSize="20px" color={editor.isActive('orderedList') ? 'primary' : 'black'} />}
         onClick={() => editor.chain().focus().toggleOrderedList().run()}
-        style={{ padding: '0' }}
       />
 
       <IconButton
         p="0px"
+        className="icon"
+        bg="transparent"
         isDisabled={isDisabled}
         aria-label="Lista desordenada"
-        bg="transparent"
-        icon={
-          <Icon
-            as={BiListUl}
-            boxSize="20px"
-            color={editor.isActive('bulletList') ? 'primary' : 'black'}
-          />
-        }
-        className="icon"
+        icon={<Icon as={BiListUl} boxSize="20px" color={editor.isActive('bulletList') ? 'primary' : 'black'} />}
         onClick={() => editor.chain().focus().toggleBulletList().run()}
-        style={{ padding: '0' }}
       />
 
       <IconButton
         p="0px"
+        className="icon"
+        bg="transparent"
         isDisabled={isDisabled}
         aria-label="Bloque código"
-        bg="transparent"
-        icon={
-          <Icon
-            as={BiCodeBlock}
-            boxSize="20px"
-            color={editor.isActive('codeBlock') ? 'primary' : 'black'}
-          />
-        }
-        className="icon"
+        icon={<Icon as={BiCodeBlock} boxSize="20px" color={editor.isActive('codeBlock') ? 'primary' : 'black'} />}
         onClick={() => editor.chain().focus().toggleCodeBlock().run()}
-        style={{ padding: '0' }}
       />
 
       <IconButton
         p="0px"
+        className="icon"
+        bg="transparent"
         isDisabled={isDisabled}
         aria-label="Deshacer"
-        bg="transparent"
-        icon={
-          <Icon
-            as={BiUndo}
-            boxSize="20px"
-            color={editor.isActive('undo') ? 'primary' : 'black'}
-          />
-        }
-        className="icon"
+        icon={<Icon as={BiUndo} boxSize="20px" color={editor.isActive('undo') ? 'primary' : 'black'} />}
         onClick={() => editor.chain().focus().undo().run()}
-        style={{ padding: '0' }}
       />
 
       <IconButton
         p="0px"
+        bg="transparent"
+        className="icon"
         isDisabled={isDisabled}
         aria-label="Rehacer"
-        bg="transparent"
-        icon={
-          <Icon
-            as={BiRedo}
-            boxSize="20px"
-            color={editor.isActive('redo') ? 'primary' : 'black'}
-          />
-        }
-        className="icon"
+        icon={<Icon as={BiRedo} boxSize="20px" color={editor.isActive('redo') ? 'primary' : 'black'} />}
         onClick={() => editor.chain().focus().redo().run()}
-        style={{ padding: '0' }}
       />
     </Flex>
   );
