127951613f1ec1343bdac476ca9560c9d371ebba
AdrianBlancoGarcia <adrian@open-bootcamp.com>
2022-08-09 11:25:29 +0200

    feat: cypress config

--- apps/campus/cypress.config.ts
+++ apps/campus/cypress.config.ts
@@ -1,9 +1,19 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  fileServerFolder: '.',
+  fixturesFolder: './cypress/fixtures',
+  video: true,
+  videosFolder: './build/cypress/apps/campus-e2e/videos',
+  screenshotsFolder: './build/cypress/apps/campus-e2e/screenshots',
+  chromeWebSecurity: false,
   e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      return require('./cypress/plugins/index.js')(on, config);
     },
+    baseUrl: 'http://localhost:3000',
+    specPattern: './cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
   },
 });
